e2ddba7583e461c73fc02576c79544bc6a4a80af
scash15-hub <527SCash@frhsd.com>
2026-03-09 11:22:07 -0400

    Refactor HTML structure and enhance keyboard button styles for improved usability

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 //AI keyboard clicked
 function keyPress(letter){
-console.log("You pressed " + letter);
+  console.log("You pressed " + letter);
+
+  // disable the button so it can't be clicked again
+  const buttons = document.querySelectorAll(".keyboard button");
+  buttons.forEach(btn => {
+    if(btn.innerText === letter){
+      btn.disabled = true;
+    }
+  });
+
+  // here you can call your hangman guess function
+  // guessLetter(letter);
 }
 //END OF AI KEYBOARD
+
+
+
+
+
