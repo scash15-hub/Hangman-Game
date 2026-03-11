0b1c1f88fe6d5659a67e85231f2aaf79c1dab0e5
scash15-hub <527SCash@frhsd.com>
2026-03-11 09:26:32 -0400

    Add background image and remove responsive styles for keyboard buttons
    - Set a background image (turf.jpg) for the body with cover size and center position.
    - Removed responsive media queries for keyboard button sizes and styles.

--- script.js
+++ script.js
@@ -1,17 +1,118 @@
+//word list
+const wordList = [
+  'Archery',
+  'Badminton',
+  'Baseball',
+  'Boxing',
+  'Cricket',
+  'Cycling',
+  'Dive',
+  'Figure Skating',
+  'Field Hockey',
+  'Fencing',
+  'Golf',
+  'Handball',
+  'Hockey',
+  'Judo',
+  'Lacrosse',
+  'Mountain Biking',
+  'Polo',
+  'Rugby',
+  'Skateboarding',
+  'Ski',
+  'Soccer',
+  'Softball',
+  'Surf',
+  'Synchronized Swimming',
+  'Tennis',
+  'Table Tennis',
+  'Track and Field',
+  'Triathlon',
+  'Volleyball',
+  'Weightlifting',
+  'Wrestling'
+];
+
+//game variables
+let selectedWord = '';
+let displayWord = '';
+let wrongGuesses = 0;
+let guessedLetters = [];
+const maxMistakes = 6;
+let winAmount = 0;
+let lossAmount = 0;
+
+//start game
+function startGame() {
+  //reset game variables
+  wrongGuesses = 0;
+  guessedLetters = [];
+  //pick a random word
+  selectedWord = getRandomWord();
+  //create starting underscores
+  displayWord = '_' .repeat(selectedWord.length);
+  //update screen
+  updateScreen();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //AI keyboard clicked
 function keyPress(letter) {
-    console.log("You pressed " + letter);
+  console.log("You pressed " + letter);
 
-    // disable the button so it can't be clicked again
-    const buttons = document.querySelectorAll(".keyboard button");
-    buttons.forEach(btn => {
-        if (btn.innerText === letter) {
-            btn.disabled = true;
-        }
-    });
+  // disable the button so it can't be clicked again
+  const buttons = document.querySelectorAll(".keyboard button");
+  buttons.forEach(btn => {
+    if (btn.innerText === letter) {
+      btn.disabled = true;
+    }
+  });
 
-    // here you can call your hangman guess function
-    // guessLetter(letter);
+  // here you can call your hangman guess function
+  // guessLetter(letter);
 }
 //END OF AI KEYBOARD
 
